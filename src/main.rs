70e172ec3ac04051687340ecfe791c6d8e9f12a5
Eyal Kalderon <ebkalderon@gmail.com>
2021-01-18 16:36:11 +0800

    Create rudimentary window with SDL2
    This commit adds a dependency on the `sdl2` Rust crate and enables the
    "bundled" and "static-link" features, which compiles both SDL2 and
    SDL2_Image from source and statically links the dependencies into the
    binary for portability, respectively.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
+use std::time::Duration;
+
+use anyhow::Error;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
 mod api;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let context = sdl2::init().map_err(Error::msg)?;
+    let video_sys = context.video().map_err(Error::msg)?;
+
+    let window = video_sys
+        .window("Disney Streaming Services", 800, 600)
+        .position_centered()
+        .build()?;
+
+    let mut canvas = window.into_canvas().present_vsync().build()?;
+    let mut events = context.event_pump().map_err(Error::msg)?;
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
 }
