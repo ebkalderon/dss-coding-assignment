622176a98bfe2231a7fd4e92029d98cf57b69989
Eyal Kalderon <ebkalderon@gmail.com>
2021-01-22 14:36:04 +0800

    Add App abstraction for managing init and main loop
    This allows us to build generic UI applications using our widget cache
    and manage the main loop.
    
    Note that this main loop current relies on the monitor V-sync to keep
    the framerate consistent. Ideally, we should try to fix our timestep in
    some other way, e.g. using `std::thread::sleep()` or using an
    accumulator. The former is the simplest approach to accomplish this, but
    results may be not so consistent, as the resolution for most OS sleep
    functions are in the order of 1-2 milliseconds, which may be too
    granular for our needs. The latter approach is more accurate and more
    efficient, but requires a little more work.
    
    Since we still need to define our widgets and get something concrete
    rendering to the screen, we will put this aside for now and focus on
    fixing our timestep in a later commit.

---
+++ src/app.rs
@@ -0,0 +1,82 @@
+//! Application state management and main loop.
+
+use anyhow::Error;
+use sdl2::event::Event;
+use sdl2::video::Window;
+use sdl2::Sdl;
+
+use crate::widget::{Widget, Widgets};
+
+/// An action to take upon receiving an SDL event.
+#[derive(Clone, Copy, Debug)]
+pub enum Action {
+    /// Continue to run the application.
+    Continue,
+    /// Shut down the application.
+    Quit,
+}
+
+/// A trait implemented by the main application state.
+pub trait State<W: Widget> {
+    /// This method is called at initialization time, before any drawing has taken place, and is
+    /// responsible for building the initial UI widget layout of the application.
+    ///
+    /// This trait method is _required_ and is guaranteed to only be called once.
+    fn initialize(&mut self, widgets: &mut Widgets<W>) -> anyhow::Result<()>;
+
+    /// This callback is called every time an [SDL event](sdl2::event::Event) is produced from the
+    /// window event loop.
+    ///
+    /// Returns an [`Action`] specifying whether the application should continue to run or quit.
+    ///
+    /// This trait method is _provided_. If it is not implemented, this method will do nothing and
+    /// always return `Action::Continue`].
+    fn handle_event(&mut self, _event: &Event, _widgets: &mut Widgets<W>) -> Action {
+        Action::Continue
+    }
+}
+
+/// Container for the main UI application.
+#[derive(Debug)]
+pub struct App<W, S> {
+    state: S,
+    widgets: Widgets<W>,
+}
+
+impl<W: Widget, S: State<W>> App<W, S> {
+    /// Creates a new `App` with the given application [`State`] and root widget.
+    pub fn new(state: S, root_widget: W) -> Self {
+        App {
+            state,
+            widgets: Widgets::new(root_widget),
+        }
+    }
+
+    /// Executes the main application loop using the given [`Sdl`](sdl2::Sdl) context and
+    /// [`Window`](sdl2::video::Window) handle.
+    ///
+    /// Returns `Ok` when the application has exited successfully, or returns `Err` if the
+    /// application failed to initialize or an SDL error was encountered.
+    pub fn run(&mut self, sdl: Sdl, window: Window) -> anyhow::Result<()> {
+        let mut events = sdl.event_pump().map_err(Error::msg)?;
+        let mut canvas = window.into_canvas().accelerated().present_vsync().build()?;
+
+        // Build and populate the `Widgets` cache.
+        self.state.initialize(&mut self.widgets)?;
+
+        'running: loop {
+            // Handle all pending SDL events.
+            for event in events.poll_iter() {
+                match self.state.handle_event(&event, &mut self.widgets) {
+                    Action::Continue => {}
+                    Action::Quit => break 'running,
+                }
+            }
+
+            // Draw the next frame to the canvas.
+            self.widgets.draw(&mut canvas)?;
+        }
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! Consumes the Disney Streaming Services (DSS) API and renders an interactive home menu.
 
+pub mod app;
 pub mod schema;
 pub mod widget;
